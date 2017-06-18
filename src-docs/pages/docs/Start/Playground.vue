eda6243123ec5388284eaddd0706a302f46c366d
David Paternina <david@fanxielab.com>
2017-06-18 17:22:54 -0500

    Responsive mixins

--- src-docs/pages/docs/Start/Playground.vue
+++ src-docs/pages/docs/Start/Playground.vue
@@ -1,80 +1,90 @@
 <template>
-    <div class="">
-        Icon FA
-        <br>
+    <div class="container">
 
-        <vui-icon icon="info-circle" type="is-small"></vui-icon>
-        <vui-icon icon="info-circle" type="is-small is-primary"></vui-icon>
-        <vui-icon icon="info-circle" type="is-small is-success"></vui-icon>
-        <vui-icon icon="info-circle" type="is-small is-info"></vui-icon>
-        <vui-icon icon="info-circle" type="is-small is-warning"></vui-icon>
-        <vui-icon icon="info-circle" type="is-small is-danger"></vui-icon>
+        <div class="columns">
 
-        <br>
-        
-        <vui-icon icon="info-circle"></vui-icon>
-        <vui-icon icon="info-circle" type="is-primary"></vui-icon>
-        <vui-icon icon="info-circle" type="is-success"></vui-icon>
-        <vui-icon icon="info-circle" type="is-info"></vui-icon>
-        <vui-icon icon="info-circle" type="is-warning"></vui-icon>
-        <vui-icon icon="info-circle" type="is-danger"></vui-icon>
+            <div class="column is-half">
 
-        <br>
-        
-        <vui-icon icon="info-circle" type="is-medium"></vui-icon>
-        <vui-icon icon="info-circle" type="is-medium is-primary"></vui-icon>
-        <vui-icon icon="info-circle" type="is-medium is-success"></vui-icon>
-        <vui-icon icon="info-circle" type="is-medium is-info"></vui-icon>
-        <vui-icon icon="info-circle" type="is-medium is-warning"></vui-icon>
-        <vui-icon icon="info-circle" type="is-medium is-danger"></vui-icon>
+                Icon FA
+                <br>
 
-        <br>
+                <vui-icon icon="info-circle" type="is-small"></vui-icon>
+                <vui-icon icon="info-circle" type="is-small is-primary"></vui-icon>
+                <vui-icon icon="info-circle" type="is-small is-success"></vui-icon>
+                <vui-icon icon="info-circle" type="is-small is-info"></vui-icon>
+                <vui-icon icon="info-circle" type="is-small is-warning"></vui-icon>
+                <vui-icon icon="info-circle" type="is-small is-danger"></vui-icon>
 
-        <vui-icon icon="info-circle" type="is-large"></vui-icon>
-        <vui-icon icon="info-circle" type="is-large is-primary"></vui-icon>
-        <vui-icon icon="info-circle" type="is-large is-success"></vui-icon>
-        <vui-icon icon="info-circle" type="is-large is-info"></vui-icon>
-        <vui-icon icon="info-circle" type="is-large is-warning"></vui-icon>
-        <vui-icon icon="info-circle" type="is-large is-danger"></vui-icon>
+                <br>
 
-        <br>
-        Icon mdi
-        <br>
+                <vui-icon icon="info-circle"></vui-icon>
+                <vui-icon icon="info-circle" type="is-primary"></vui-icon>
+                <vui-icon icon="info-circle" type="is-success"></vui-icon>
+                <vui-icon icon="info-circle" type="is-info"></vui-icon>
+                <vui-icon icon="info-circle" type="is-warning"></vui-icon>
+                <vui-icon icon="info-circle" type="is-danger"></vui-icon>
 
-        <vui-icon pack="mdi" icon="info" type="is-small"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-small is-primary"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-small is-success"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-small is-info"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-small is-warning"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-small is-danger"></vui-icon>
+                <br>
 
-        <br>
+                <vui-icon icon="info-circle" type="is-medium"></vui-icon>
+                <vui-icon icon="info-circle" type="is-medium is-primary"></vui-icon>
+                <vui-icon icon="info-circle" type="is-medium is-success"></vui-icon>
+                <vui-icon icon="info-circle" type="is-medium is-info"></vui-icon>
+                <vui-icon icon="info-circle" type="is-medium is-warning"></vui-icon>
+                <vui-icon icon="info-circle" type="is-medium is-danger"></vui-icon>
 
-        <vui-icon pack="mdi" icon="info"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-primary"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-success"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-info"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-warning"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-danger"></vui-icon>
+                <br>
 
-        <br>
+                <vui-icon icon="info-circle" type="is-large"></vui-icon>
+                <vui-icon icon="info-circle" type="is-large is-primary"></vui-icon>
+                <vui-icon icon="info-circle" type="is-large is-success"></vui-icon>
+                <vui-icon icon="info-circle" type="is-large is-info"></vui-icon>
+                <vui-icon icon="info-circle" type="is-large is-warning"></vui-icon>
+                <vui-icon icon="info-circle" type="is-large is-danger"></vui-icon>
 
-        <vui-icon pack="mdi" icon="info" type="is-medium"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-medium is-primary"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-medium is-success"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-medium is-info"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-medium is-warning"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-medium is-danger"></vui-icon>
+            </div>
+            <div class="column is-half">
+                Icon mdi
+                <br>
 
-        <br>
+                <vui-icon pack="mdi" icon="info" type="is-small"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-small is-primary"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-small is-success"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-small is-info"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-small is-warning"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-small is-danger"></vui-icon>
 
-        <vui-icon pack="mdi" icon="info" type="is-large"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-large is-primary"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-large is-success"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-large is-info"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-large is-warning"></vui-icon>
-        <vui-icon pack="mdi" icon="info" type="is-large is-danger"></vui-icon>
-        
+                <br>
+
+                <vui-icon pack="mdi" icon="info"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-primary"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-success"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-info"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-warning"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-danger"></vui-icon>
+
+                <br>
+
+                <vui-icon pack="mdi" icon="info" type="is-medium"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-medium is-primary"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-medium is-success"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-medium is-info"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-medium is-warning"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-medium is-danger"></vui-icon>
+
+                <br>
+
+                <vui-icon pack="mdi" icon="info" type="is-large"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-large is-primary"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-large is-success"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-large is-info"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-large is-warning"></vui-icon>
+                <vui-icon pack="mdi" icon="info" type="is-large is-danger"></vui-icon>
+
+            </div>
+
+        </div>
+
     </div>
 </template>
 <script>
